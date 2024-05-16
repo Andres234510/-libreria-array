1f684e7edf2ad043af2e047ddbe8e0d6eaf350a9
SENA <andres2345v@gmail.com>
2024-05-16 11:18:56 -0500

    añadi el primer libro

---
+++ index.js
@@ -0,0 +1,21 @@
+let libro = [
+    {
+        titulo : "Harry Potter y la piedra filosofal",
+        autor : "J.K. Rowling",
+        genero : "fantasia",
+        idioma : "Ingles Britanico",
+        precio : "$ 47.000 Col.",
+        formato : "Tapa blanda bolsillo",
+        ISBN : "9788498382662",
+        descripcion : "Con las manos temblorosas, Harry le dio la vuelta al sobre y vio un  sello de lacre púrpura con un escudo de armas: un león, un águila, un tejón y una serpiente, que rodeaban una gran letra H. Harry Potter nunca ha oído hablar de Hogwarts hasta que empiezan a caer  cartas en el felpudo del número 4 de Privet Drive. Llevan la dirección  escrita con tinta verde en un sobre de pergamino amarillento con un  sello de lacre púrpura, y sus horripilantes tíos se apresuran a  confiscarlas. Más tarde, el día que Harry cumple once años, Rubeus  Hagrid, un hombre gigantesco cuyos ojos brillan como escarabajos negros,  irrumpe con una noticia extraordinaria: Harry Potter es un mago, y le  han concedido una plaza en el Colegio Hogwarts de Magia y Hechicería. ¡Está a punto de comenzar una aventura increíble!",
+        estado : "Nuevo",
+        ubicacion : "la casa del libro",
+        fechaPublicacion :  "26 June 1997",
+        dimensiones : {
+            ancho : "13.7 cm",
+            alto : "22 cm",
+            grueso : "2.2 cm",
+        },
+        peso: "397 gr"
+    },
+]
